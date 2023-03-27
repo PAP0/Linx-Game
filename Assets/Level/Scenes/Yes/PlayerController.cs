796765c23bdfa41a13808a2e398acf14eb5f6ce6
Bjornraaf <30087@ma-web.nl>
2023-03-27 13:13:30 +0200

    Cleanup & Script implementation

--- Assets/Level/Scenes/Yes/PlayerController.cs
+++ Assets/Level/Scenes/Yes/PlayerController.cs
@@ -7,19 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float Speed = 5f;
-    [SerializeField] private bool IsPC;
 
     private Vector2 Movement;
-    private Vector2 MouseLook;
     private Vector2 JoystickLook;
-    private Vector3 RotationTarget;
+
     public void OnMove(InputAction.CallbackContext context)
     {
         Movement = context.ReadValue<Vector2>();
-    }
-    public void OnMouseLook(InputAction.CallbackContext context)
-    {
-        MouseLook = context.ReadValue<Vector2>();
     }
     public void OnJoystickLook(InputAction.CallbackContext context)
     {
@@ -28,31 +22,16 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (IsPC)
+        if (JoystickLook.x == 0 && JoystickLook.y == 0)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(MouseLook);
-
-            if(Physics.Raycast(ray, out hit))
-            {
-                RotationTarget = hit.point;
-            }
-            RotatePlayerWithAim();
+            MovePlayer();
         }
         else
         {
-            if (JoystickLook.x == 0 && JoystickLook.y == 0)
-            {
-                MovePlayer();
-            }
-            else
-            {
-                RotatePlayerWithAim();
-            }
+            RotatePlayerWithAim();
         }
-        
     }
-    public void MovePlayer()
+    private void MovePlayer()
     {
         Vector3 move = new Vector3(Movement.x, 0f, Movement.y);
 
@@ -62,33 +41,18 @@ public class PlayerController : MonoBehaviour
         }
         transform.Translate(move * Speed * Time.deltaTime, Space.World);
     }
-    public void RotatePlayerWithAim()
+
+    private void RotatePlayerWithAim()
     {
-        if (IsPC)
+        Vector3 aimDirection = new Vector3(JoystickLook.x, 0f, JoystickLook.y);
+
+        if (aimDirection != Vector3.zero)
         {
-            var lookPos = RotationTarget - transform.position;
-            lookPos.y = 0;
-            var rotation = Quaternion.LookRotation(lookPos);
-
-            Vector3 aimDirection = new Vector3(RotationTarget.x, 0f, RotationTarget.z);
-
-            if (aimDirection != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
-            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
         }
-        else
-        {
-            Vector3 aimDirection = new Vector3(JoystickLook.x, 0f, JoystickLook.y);
 
-            if (aimDirection != Vector3.zero)
-            {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
-            }
-        }
         Vector3 movement = new Vector3(Movement.x, 0f, Movement.y);
 
         transform.Translate(movement * Speed * Time.deltaTime, Space.World);
-
     }
 }
