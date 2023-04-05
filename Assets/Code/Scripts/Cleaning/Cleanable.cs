13ecb2bdaa95c14a757eb0b8bfdb7a0f5fe7029e
Bjornraaf <30087@ma-web.nl>
2023-04-05 15:30:57 +0200

    Dirtbrush works with player instead of mouse input

--- Assets/Code/Scripts/Cleaning/Cleanable.cs
+++ Assets/Code/Scripts/Cleaning/Cleanable.cs
@@ -1,22 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.InputSystem;
 public class Cleanable : MonoBehaviour
 {
 
     [SerializeField] private Texture2D DirtMaskTextureBase;
     [SerializeField] private Texture2D DirtBrush;
     [SerializeField] private Material Material;
-    //[SerializeField] private TextMeshProUGUI UIText;
+
+    private GameObject Player;
 
     private Texture2D DirtMaskTexture;
+
+    private Vector2Int LastPaintPixelPosition;
+
     private float DirtAmountTotal;
     private float DirtAmount;
-    private Vector2Int LastPaintPixelPosition;
+
+    private bool IsCleanable;
+
+    public void OnClean(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        IsCleanable = true;
+    }
 
     private void Awake()
     {
+
         DirtMaskTexture = new Texture2D(DirtMaskTextureBase.width, DirtMaskTextureBase.height);
         DirtMaskTexture.SetPixels(DirtMaskTextureBase.GetPixels());
         DirtMaskTexture.Apply();
@@ -31,80 +42,96 @@ public class Cleanable : MonoBehaviour
             }
         }
         DirtAmount = DirtAmountTotal;
-
-        //FunctionPeriodic.Create(() => {
-        //    UIText.text = Mathf.RoundToInt(GetDirtAmount() * 100f) + "%";
-        //}, .03f);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        //if (IsCleanable)
+        //{
+        //    Clean();
+        //}
+        //else
+        //{
+        //    IsCleanable = false;
+        //}
+        Clean();
+    }
+    private void Clean()
+    {
+        if (GetDirtAmount() <= 0)
         {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit))
+            gameObject.SetActive(false);
+        }
+        //UIText.text = GetDirtAmount() + "%";
+        Player = GameObject.Find("SmallPlayer(Clone)");
+        Vector3 playerPosition = Player.transform.position;
+        if (Physics.Raycast(playerPosition, -Vector3.up, out RaycastHit raycastHit))
+        {
+            Vector2 textureCoord = raycastHit.textureCoord;
+
+            int pixelX = (int)(textureCoord.x * DirtMaskTexture.width);
+            int pixelY = (int)(textureCoord.y * DirtMaskTexture.height);
+
+            Vector2Int paintPixelPosition = new Vector2Int(pixelX, pixelY);
+            //Debug.Log("UV: " + textureCoord + "; Pixels: " + paintPixelPosition);
+
+            int paintPixelDistance = Mathf.Abs(paintPixelPosition.x - LastPaintPixelPosition.x) + Mathf.Abs(paintPixelPosition.y - LastPaintPixelPosition.y);
+            int maxPaintDistance = 0;
+            if (paintPixelDistance < maxPaintDistance)
             {
-                Vector2 textureCoord = raycastHit.textureCoord;
+                // Painting too close to last position
+                return;
+            }
+            LastPaintPixelPosition = paintPixelPosition;
 
-                int pixelX = (int)(textureCoord.x * DirtMaskTexture.width);
-                int pixelY = (int)(textureCoord.y * DirtMaskTexture.height);
+            /*
+            // Paint Square in Dirt Mask
+            int squareSize = 32;
+            int pixelXOffset = pixelX - (dirtBrush.width / 2);
+            int pixelYOffset = pixelY - (dirtBrush.height / 2);
 
-                Vector2Int paintPixelPosition = new Vector2Int(pixelX, pixelY);
-                //Debug.Log("UV: " + textureCoord + "; Pixels: " + paintPixelPosition);
-
-                int paintPixelDistance = Mathf.Abs(paintPixelPosition.x - LastPaintPixelPosition.x) + Mathf.Abs(paintPixelPosition.y - LastPaintPixelPosition.y);
-                int maxPaintDistance = 7;
-                if (paintPixelDistance < maxPaintDistance)
-                {
-                    // Painting too close to last position
-                    return;
+            for (int x = 0; x < squareSize; x++) {
+                for (int y = 0; y < squareSize; y++) {
+                    dirtMaskTexture.SetPixel(
+                        pixelXOffset + x,
+                        pixelYOffset + y,
+                        Color.black
+                    );
                 }
-                LastPaintPixelPosition = paintPixelPosition;
+            }
+            //*/
+            int pixelXOffset = pixelX - (DirtBrush.width / 2);
+            int pixelYOffset = pixelY - (DirtBrush.height / 2);
 
-                /*
-                // Paint Square in Dirt Mask
-                int squareSize = 32;
-                int pixelXOffset = pixelX - (dirtBrush.width / 2);
-                int pixelYOffset = pixelY - (dirtBrush.height / 2);
-
-                for (int x = 0; x < squareSize; x++) {
-                    for (int y = 0; y < squareSize; y++) {
-                        dirtMaskTexture.SetPixel(
-                            pixelXOffset + x,
-                            pixelYOffset + y,
-                            Color.black
-                        );
-                    }
-                }
-                //*/
-                int pixelXOffset = pixelX - (DirtBrush.width / 2);
-                int pixelYOffset = pixelY - (DirtBrush.height / 2);
-
-                for (int x = 0; x < DirtBrush.width; x++)
+            for (int x = 0; x < DirtBrush.width; x++)
+            {
+                for (int y = 0; y < DirtBrush.height; y++)
                 {
-                    for (int y = 0; y < DirtBrush.height; y++)
+                    int pixelXPos = pixelXOffset + x;
+                    int pixelYPos = pixelYOffset + y;
+
+                    if (pixelXPos >= 0 && pixelXPos < DirtMaskTexture.width && pixelYPos >= 0 && pixelYPos < DirtMaskTexture.height)
                     {
                         Color pixelDirt = DirtBrush.GetPixel(x, y);
-                        Color pixelDirtMask = DirtMaskTexture.GetPixel(pixelXOffset + x, pixelYOffset + y);
+                        Color pixelDirtMask = DirtMaskTexture.GetPixel(pixelXPos, pixelYPos);
 
                         float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
                         DirtAmount -= removedAmount;
 
                         DirtMaskTexture.SetPixel(
-                            pixelXOffset + x,
-                            pixelYOffset + y,
+                            pixelXPos,
+                            pixelYPos,
                             new Color(0, pixelDirtMask.g * pixelDirt.g, 0)
                         );
                     }
                 }
-                DirtMaskTexture.Apply();
             }
+            DirtMaskTexture.Apply();
         }
-
     }
-
     private float GetDirtAmount()
     {
-        return this.DirtAmount / DirtAmountTotal;
+        return Mathf.RoundToInt(DirtAmount / DirtAmountTotal * 100);
     }
 
 }
