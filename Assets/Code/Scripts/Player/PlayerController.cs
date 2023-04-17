95845c36a0a178cbaff0ec75cc0f915cbc3aadff
PAP0 <29280@ma-web.nl>
2023-04-17 09:37:57 +0200

    Deleted line that set held object as parent

--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -206,9 +206,6 @@ public class PlayerController : MonoBehaviour
                     {
                         GrabSpeed = 0.5f;
                     }
-
-                    // Set the parent of the held object to be the player
-                    HeldObject.transform.parent = transform;
                 }
             }
             else if (hit.collider.CompareTag("Player"))
