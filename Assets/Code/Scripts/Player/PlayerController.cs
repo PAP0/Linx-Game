5e70cf21f0c44116c3103dd3bfccd2bff0d0db9d
Bjornraaf <30087@ma-web.nl>
2023-05-08 13:15:24 +0200

    pulling v2

--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -19,15 +19,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float JumpForce = 1.5f;
 
     [Header("Grabbing")]
+    [SerializeField] private float MaxGrabDistance = 2f;   // Maximum distance the player can grab an object from.
     [SerializeField] private float MaxGrabWeight = 50.0f;
+    [SerializeField] private LayerMask GrabMask;          // Layer mask for objects that can be grabbed
+    [SerializeField] private bool IsGrabbing;
+
 
     [Header("Animation")] 
     [SerializeField] private Animator PlayerAnimator;
 
     private readonly float GravityMagnitude = Physics.gravity.y;
     private float Velocity;
+
     private Rigidbody RB;
 
+    private GameObject heldObject;       // Object currently being held by the player
+
+    private Vector3 holdOffset;          // Offset between the player's hand and the center of the held object
     private Vector2 Movement;
 
     private void Awake()
@@ -39,6 +47,20 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         Movement = context.ReadValue<Vector2>();
+    }
+    public void OnGrab(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        // Check if the character is already holding an object
+        if (IsGrabbing)
+        {
+            ReleaseObject();
+        }
+        else
+        {
+            GrabObject();
+        }
     }
 
     private void Update()
@@ -53,6 +75,12 @@ public class PlayerController : MonoBehaviour
         else
         {
             PlayerAnimator.SetBool("IsRunning", false);
+        }
+
+        // If the player is holding an object, update its position
+        if (heldObject != null)
+        {
+            UpdateHeldObjectPosition();
         }
     }
 
@@ -73,28 +101,109 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(Movement.x, Velocity, Movement.y);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(Movement.x, 0, Movement.y)), 0.15f);
         Controller.Move(move * Speed * Time.deltaTime);
+
+        if (!IsGrabbing)
+        {
+            return;
+        }
+        else
+        {
+            Quaternion lookRotation = Quaternion.LookRotation((heldObject.transform.position - transform.position).normalized);
+            transform.rotation = lookRotation;
+        }
     }
 
     private void OnControllerColliderHit(ControllerColliderHit controllerHit)
     {
         Rigidbody rigidbody = controllerHit.collider.attachedRigidbody;
+        Vector3 forceDirection;
 
         if (rigidbody != null && controllerHit.gameObject.CompareTag("Grabbable"))
         {
             if (MaxGrabWeight >= rigidbody.mass)
             {
-                Vector3 forceDirection = controllerHit.gameObject.transform.position - transform.position;
+                forceDirection = controllerHit.gameObject.transform.position - transform.position;
                 forceDirection.y = 0;
                 forceDirection.Normalize();
 
                 rigidbody.AddForceAtPosition(forceDirection * ((MaxGrabWeight - rigidbody.mass) / 100 + 0.5f), transform.position, ForceMode.Impulse);
             }
         }
-        
     }
-    private void Drag()
-    {
-        
-    }
+    //void GrabObject()
+    //{
+    //    // Raycast to find the closest grabbable object within range
+    //    RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, maxGrabDistance, grabMask);
+    //    if (hits.Length > 0)
+    //    {
+    //        // Find the closest object that is not the player or the player's child
+    //        GameObject closestObject = null;
+    //        float closestDistance = Mathf.Infinity;
+    //        foreach (RaycastHit hit in hits)
+    //        {
+    //            float distance = Vector3.Distance(transform.position, hit.transform.position);
+    //            if (distance < closestDistance && hit.transform.gameObject != gameObject && hit.transform.parent != transform)
+    //            {
+    //                closestObject = hit.transform.gameObject;
+    //                closestDistance = distance;
+    //            }
+    //        }
 
+    //        // If a grabbable object was found, hold onto it
+    //        if (closestObject != null)
+    //        {
+    //            IsGrabbing = true;
+    //            heldObject = closestObject;
+    //            holdOffset = transform.position - closestObject.transform.position;
+    //        }
+    //    }
+    //}
+    void GrabObject()
+    {
+        // Raycast to find the closest grabbable object within range
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, MaxGrabDistance, GrabMask);
+        if (hits.Length > 0)
+        {
+            // Find the closest object that is not the player or the player's child
+            GameObject closestObject = null;
+            float closestDistance = Mathf.Infinity;
+            foreach (RaycastHit hit in hits)
+            {
+                float distance = Vector3.Distance(transform.position, hit.transform.position);
+                if (distance < closestDistance && hit.transform.gameObject != gameObject && hit.transform.parent != transform)
+                {
+                    closestObject = hit.transform.gameObject;
+                    closestDistance = distance;
+                }
+            }
+
+            // If a grabbable object was found, hold onto it
+            if (closestObject != null)
+            {
+                IsGrabbing = true;
+                heldObject = closestObject;
+                holdOffset = transform.position - closestObject.transform.position;
+            }
+        }
+    }
+    void ReleaseObject()
+    {
+        heldObject = null;
+        IsGrabbing = false;
+    }
+    //void UpdateHeldObjectPosition()
+    //{
+    //    // Move the object towards the player's hand
+    //    Vector3 targetPosition = transform.position - holdOffset;
+    //    heldObject.transform.position = Vector3.MoveTowards(heldObject.transform.position, targetPosition, grabSpeed * Time.deltaTime);
+    //}
+    void UpdateHeldObjectPosition()
+    {
+        Rigidbody rigidbody = heldObject.GetComponent<Rigidbody>();
+        Vector3 forceDirection = heldObject.transform.position - transform.position;
+
+        forceDirection.y = 0;
+        forceDirection.Normalize();
+        rigidbody.AddForceAtPosition(-forceDirection * ((MaxGrabWeight - rigidbody.mass) / 100 + 0.5f), heldObject.transform.position, ForceMode.Impulse);
+    }
 }
