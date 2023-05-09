949f79bd4637b7dfceafa3db7ba39160b78f129c
PAP0 <29280@ma-web.nl>
2023-05-09 13:53:15 +0200

    Join In Feature Done
    -Timer only starts when 2 players are joined
    -Elevator opens when timer is started

--- Assets/Code/Scripts/Co-op/PlayerJoinManager.cs
+++ Assets/Code/Scripts/Co-op/PlayerJoinManager.cs
@@ -21,10 +21,14 @@ public class PlayerJoinManager : MonoBehaviour
 { 
     [Tooltip("The Player Input Manager")]
     [SerializeField] private PlayerInputManager PlayerInputManager; // A reference to the PlayerInputManager.
+    [Tooltip("The Timer Script")]
+    [SerializeField] private Timer TimerScript; // A reference to the Timer script.
     [Tooltip("The Player Prefabs")]
     [SerializeField] private GameObject[] PlayerPrefabs; // An array of player prefabs to choose from.
     [Tooltip("The spawn points for players")]
     [SerializeField] private Transform[] SpawnPoints;
+    [Tooltip("The Press To Join Hud Elements")]
+    [SerializeField] private GameObject[] HudJoinElements; // An array of hud elements that are turned off depending on the player count.
     private int CurrentPrefabIndex = 0; // Index of current prefab to use.
     private int NumPlayersJoined = 0; // Number of players that have joined.
 
@@ -38,16 +42,11 @@ public class PlayerJoinManager : MonoBehaviour
 
         // Skip the first prefab of the array so that it doesn't spawn in 2 players with the same prefab.
         CurrentPrefabIndex = 1; // Start with one player already joined.
-        Time.timeScale = 0.000001f;
+        //Time.timeScale = 0.000001f;
     }
 
     public void OnPlayerJoined()
-    {  
-        if (GetComponent<PlayerInputManager>().playerCount == 2)
-        {
-            Time.timeScale = 1f;
-        }
-
+    {
         // Set the new player prefab.
         PlayerInputManager.playerPrefab = PlayerPrefabs[CurrentPrefabIndex];
 
@@ -57,5 +56,16 @@ public class PlayerJoinManager : MonoBehaviour
 
         // Set the prefab array index to the next prefab in the array.
         CurrentPrefabIndex = (CurrentPrefabIndex + 1) % PlayerPrefabs.Length;
+
+        if (PlayerInputManager.playerCount > 0)
+        {
+            HudJoinElements[0].SetActive(false);
+
+            if (PlayerInputManager.playerCount == 2)
+            {
+                HudJoinElements[1].SetActive(false);
+                TimerScript.TimerOn = true;
+            }
+        }
     }
 }
