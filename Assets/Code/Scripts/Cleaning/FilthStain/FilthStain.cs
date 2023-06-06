3e43c7bdf8d4a431874922fb775cbda6c8b889b0
Ties-Postma <28114@ma-web.nl>
2023-06-06 12:11:56 +0200

    Made button script
    Made button script that triggers an animation of the mechanical doors that opens it.

--- Assets/Code/Scripts/Cleaning/FilthStain/FilthStain.cs
+++ Assets/Code/Scripts/Cleaning/FilthStain/FilthStain.cs
@@ -6,35 +6,61 @@ using Random = Unity.Mathematics.Random;
 
 public class FilthStain : MonoBehaviour
 {
-    public Vacuum vacuumScript;
+    [SerializeField] private float Range;       // The range within which the object is considered in range
+    [SerializeField] private ScoreScriptableObject ScoreHolder;
+
+    [Header("Blood")]
+    [SerializeField] private bool IsBloodStain;
+    [SerializeField] private bool IsSoaped;
 
     [Header("Garbage")]
-    public bool IsExposed;
-    public bool HiddenFilth;
+    [SerializeField] private bool IsGarbagePatch;
+    [SerializeField] private bool IsBrushed;
+    
+    private Animator BloodAnimator;
 
-    [SerializeField] private float Range;
-    [SerializeField] private ScoreScriptableObject ScoreHolder;
+    private void Start()
+    {
+        BloodAnimator = gameObject.GetComponent<Animator>();
+    }
 
     void Update()
     {
+        // Get the object with the specified tag
         GameObject vacuumGuy = GameObject.FindGameObjectWithTag("AbilityBarrierVac");
+        GameObject mopGuy = GameObject.FindGameObjectWithTag("AbilityBarrierMop");
+        // Calculate the distance between the reference object and the current object
         float distanceVacuum = Vector3.Distance(transform.position, vacuumGuy.transform.position);
-        if (distanceVacuum <= Range && vacuumScript.IsSucking && !HiddenFilth)
+        float distanceMop = Vector3.Distance(transform.position, mopGuy.transform.position);
+        if (distanceMop <= Range && IsGarbagePatch)
         {
-            Destroy(gameObject);
-            ScoreHolder.ScoreValue++;
+            IsBrushed = true;
         }
-
-        if (distanceVacuum <= Range && vacuumScript.IsSucking && IsExposed && IsExposed)
+        if (distanceVacuum <= Range && IsGarbagePatch && IsBrushed)
         {
             Destroy(gameObject);
-            ScoreHolder.ScoreValue++;
+        }
+        if (distanceVacuum <= Range && IsBloodStain)
+        {
+            IsSoaped = true;
+        }
+        if (distanceMop <= Range && IsBloodStain && IsSoaped)
+        {   
+            StartCoroutine(Fade());
         }
     }
-
+    
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.gray;
+        Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, Range);
+    }
+
+    IEnumerator Fade()
+    {
+        BloodAnimator.SetTrigger("IsSoaped");
+        yield return new WaitForSeconds(3f);
+        ScoreHolder.ScoreValue++;
+        Destroy(gameObject);
     }
 }
