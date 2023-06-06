7b1cddf5bf799e22df6e8ec4e8a342c6dc0a6fb3
Bjornraaf <30087@ma-web.nl>
2023-06-07 01:44:59 +0200

    Code Refactoring
    The refactored code by the agreed code conventions are the following Scripts:
    - FilthSpawner
    - ScorePrinter + ScoreScriptableObject

--- Assets/Code/Scripts/Cleaning/FilthSpawner.cs
+++ Assets/Code/Scripts/Cleaning/FilthSpawner.cs
@@ -1,26 +1,53 @@
-using System.Collections;
 using UnityEngine;
 
+/// <summary>
+/// This script spawns objects in random positions within specified constraints.
+/// </summary>
 public class FilthSpawner : MonoBehaviour
 {
-    [Header("Spawn Settings")]
+    #region UnityEvents
+
+    [Header("References")]
+    [Tooltip("Reference to the ScriptableObject that holds the CurrentScore.")]
     [SerializeField] private ScoreScriptableObject Score;
 
     [Header("Spawn Settings")]
+    [Tooltip("The amount of chance there is for a object to spawn at a position.")]
     [SerializeField] private float SpawnChance;
+
+    [Tooltip("The Different objects that can be spawned")]
     [SerializeField] private GameObject[] SpawnObjects;
     
     [Header("Raycast Settings")]
+    [Tooltip("The Distance the object can be spawned from each other.")]
     [SerializeField] private float Distance;
+
+    [Tooltip("The Maximum Range for the FilthSpawners raycast")]
     [SerializeField] private float Range;
+
+    [Tooltip("The layers the objects can be spawn on")]
     [SerializeField] private LayerMask Mask;
 
     [Header("Spawn Contraints")]
+    [Tooltip("The minimum X & Z positions the FilthSpawner can spawn between.")]
     [SerializeField] private Vector2 MinimumSpawnPosition;
+
+    [Tooltip("The maximum X & Z positions the FilthSpawner can spawn between.")]
     [SerializeField] private Vector2 MaximumSpawnPosition;
 
+    [Tooltip("Array that holds all the spawned FithStains in the game")]
     public FilthStain[] filthStain;
 
+    // Randomizes the SpawnObjects
+    private int RandomIndex;
+
+    #endregion
+
+    #region UnityEvents
+
+    /// <summary>
+    /// Finds the Vacuum player in the scene for every FiltStain..
+    /// </summary>
     public void FindVacuumGuy()
     {
         filthStain = FindObjectsOfType<FilthStain>();
@@ -30,31 +57,38 @@ public class FilthSpawner : MonoBehaviour
         }
     }
 
-    private int RandomIndex;
-    // Start is called before the first frame update
-    void Start()
+    // When the script is initialized.
+    private void Start()
     {
+        // Resets the MaxScore/TotalObjects in the Scene.
         Score.TotalObjects = 0;
 
         Spawn();
     }
 
+    // Spawns objects in random positions within specified constraints.
     private void Spawn()
     {
+        // Checks every X position between the Minimum, Maximum & Distance contraint.
         for (float x = MinimumSpawnPosition.x; x < MaximumSpawnPosition.x; x += Distance)
         {
+            // Checks every Z position between the Minimum, Maximum & Distance contraint.
             for (float z = MinimumSpawnPosition.y; z < MaximumSpawnPosition.y; z += Distance)
             {
                 if (Physics.Raycast(new Vector3(x, 0, z), Vector3.down, out RaycastHit hit, Range, Mask))
                 {
+                    // Checks if the SpawnChance is bigger than the randomized number.
                     if (SpawnChance > Random.Range(0, 101))
                     {
                         Score.TotalObjects++;
+                        // Spawns the object at the current randomized position.
                         Instantiate(SpawnObjects[RandomIndex], new Vector3(hit.point.x, hit.point.y + 0.03f, hit.point.z), Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), transform);
+                        // Picks a random object from the SpawnObjects.
                         RandomIndex = Random.Range(0, SpawnObjects.Length);
                     }
                 }
             }
         }
     }
+    #endregion
 }
--- Assets/Code/Scripts/UI/Score/ScorePrinter.cs
+++ Assets/Code/Scripts/UI/Score/ScorePrinter.cs
@@ -1,23 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This script sets the MaxScore & Prints out the CurrentScore to the specified UI Slider.
+/// </summary>
 public class ScorePrinter : MonoBehaviour
 {
-    [Header("Scriptable Object")]
-    [SerializeField] private ScoreScriptableObject ScoreHolder;
-    [Header("Scores")]
-    [SerializeField] private Slider ScoreSlider;
-    //[SerializeField] private TMP_Text EndScoreText;
+    #region Variables
 
+    [Header("References")]
+    [Tooltip("Reference to the ScriptableObject that holds the CurrentScore.")]
+    [SerializeField] private ScoreScriptableObject ScoreHolder;
+
+    [Tooltip("Reference to the slider that will show the CurrentScore in the Game.")]
+    [SerializeField] private Slider ScoreSlider;
+
+    #endregion
+
+    #region UnityEvents
+
+    // When the script is initialized.
     private void Start()
     {
+        // Resets the score at the start of the game.
         ScoreHolder.ScoreValue = 0;
 
+        // Sets the MaxValue to the amount of objects spawned in.
+        ScoreSlider.maxValue = ScoreHolder.TotalObjects;
     }
 
+    // Checks every frame.
     private void Update()
     {
-        ScoreSlider.maxValue = ScoreHolder.TotalObjects;
+        // Prints the updated CurrentScore taken from the ScriptableObject.
         ScoreSlider.value = ScoreHolder.ScoreValue;
     }
+
+    #endregion
 }
